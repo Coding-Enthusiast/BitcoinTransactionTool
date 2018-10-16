973f95bf49b4b17fc2af27cc7ab9e1e07950dfd8
CodingEnthusiast <CodingEnthusiast@protonmail.com>
2018-10-16 15:01:53 +0330

    Added ViewModelBase for easy error|stat reports.

---
+++ CommonLibrary/ViewModelBase.cs
@@ -0,0 +1,42 @@
+﻿namespace CommonLibrary
+{
+    public class ViewModelBase : InpcBase
+    {
+        /// <summary>
+        /// Used for changing the visibility of error message TextBox.
+        /// </summary>
+        public bool IsErrorMsgVisible
+        {
+            get { return isErrorMsgVisible; }
+            private set { SetField(ref isErrorMsgVisible, value); }
+        }
+        private bool isErrorMsgVisible;
+
+        /// <summary>
+        /// String containing all the errors.
+        /// </summary>
+        public string Errors
+        {
+            get { return errors; }
+            set
+            {
+                if (SetField(ref errors, value))
+                {
+                    IsErrorMsgVisible = !string.IsNullOrEmpty(value);
+                }
+            }
+        }
+        private string errors;
+
+        /// <summary>
+        /// Status, showing current action being performed.
+        /// </summary>
+        public string Status
+        {
+            get { return status; }
+            set { SetField(ref status, value); }
+        }
+        private string status;
+
+    }
+}
