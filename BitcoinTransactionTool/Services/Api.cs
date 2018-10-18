61631fc66c46985546bffc8502e7877f85397242
CodingEnthusiast <CodingEnthusiast@protonmail.com>
2018-10-18 18:20:12 +0330

    Changing the way Api calls work.

---
+++ BitcoinTransactionTool/Services/Api.cs
@@ -0,0 +1,54 @@
+﻿using BitcoinTransactionTool.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BitcoinTransactionTool.Services
+{
+    public enum TxApiNames
+    {
+        BlockCypher,
+        BlockchainInfo,
+    }
+
+    public abstract class Api
+    {
+        protected async Task<Response<JObject>> SendApiRequestAsync(string url)
+        {
+            Response<JObject> resp = new Response<JObject>();
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string result = await client.GetStringAsync(url);
+                    if (result.StartsWith("["))
+                    {
+                        resp.Result = new JObject
+                        {
+                            { "Result", JArray.Parse(result) }
+                        };
+                    }
+                    else
+                    {
+                        resp.Result = JObject.Parse(result);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string errMsg = (ex.InnerException == null) ?
+                        ex.Message :
+                        ex.Message + Environment.NewLine + ex.InnerException.Message;
+                    resp.Errors.Add(errMsg);
+                }
+            }
+            return resp;
+        }
+    }
+
+    public abstract class TransactionApi : Api
+    {
+        public abstract Task<Response<List<UTXO>>> GetUTXO(List<SendingAddress> addrList);
+    }
+}
---
+++ BitcoinTransactionTool/Services/Response.cs
@@ -0,0 +1,13 @@
+﻿namespace BitcoinTransactionTool.Services
+{
+    public class Response<T> : Response
+    {
+        public T Result { get; set; }
+    }
+
+
+    public class Response
+    {
+        public ErrorCollection Errors { get; } = new ErrorCollection();
+    }
+}
