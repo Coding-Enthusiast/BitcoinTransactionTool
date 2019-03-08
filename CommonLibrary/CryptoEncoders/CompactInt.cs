f24ff7d783269917c0c1423257c065583b3e5a02
CodingEnthusiast <CodingEnthusiast@protonmail.com>
2019-03-08 19:10:23 +0330

    Improve CompactInt encoder.

--- CommonLibrary/CryptoEncoders/CompactInt.cs
+++ CommonLibrary/CryptoEncoders/CompactInt.cs
@@ -18,31 +18,84 @@ namespace CommonLibrary.CryptoEncoders
         }
 
         /// <summary>
+        /// Initializes a new instance of <see cref="CompactInt"/> to the value indicated by a 32-bit signed integer.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        /// <param name="value">Value to use (must be positive).</param>
+        public CompactInt(int value)
+        {
+            if (value < 0)
+                throw new ArgumentOutOfRangeException(nameof(value), "Length can not be negative!");
+
+            SetFields((ulong)value);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="CompactInt"/> to the value indicated by a 32-bit unsigned integer.
+        /// </summary>
+        /// <param name="value">The 32-bit unsigned integer to use.</param>
+        public CompactInt(uint value) : this((ulong)value)
+        {
+        }
+
+        /// <summary>
         /// Initializes a new instance of <see cref="CompactInt"/> to the value indicated by a 64-bit unsigned integer.
         /// </summary>
-        /// <param name="number">A 64-bit unsigned integer.</param>
+        /// <param name="number">The 64-bit unsigned integer to use.</param>
         public CompactInt(ulong number)
         {
-            Number = number;
-            Bytes = Write(number);
+            SetFields(number);
         }
 
         /// <summary>
         /// Initializes a new instance of <see cref="CompactInt"/> to the value indicated by an array of bytes.
+        /// The byte array can contain extra bytes at the end which will be ignored.
         /// </summary>
-        /// <exception cref="ArgumentException"/>
         /// <exception cref="ArgumentNullException"/>
-        /// <param name="ba">An array of bytes.</param>
-        public CompactInt(byte[] ba)
+        /// <exception cref="FormatException"/>
+        /// <param name="data">An array of bytes in little-endian order containing a <see cref="CompactInt"/>.</param>
+        public CompactInt(byte[] data)
         {
-            if (ba == null)
-            {
-                throw new ArgumentNullException(nameof(ba), "Input byte array can not be null!");
-            }
+            if (data == null || data.Length == 0)
+                throw new ArgumentNullException(nameof(data), "Input byte array can not be null!");
 
-            int size = 0;
-            Number = Read(ba, ref size);
-            Bytes = ba.SubArray(0, size);
+
+            if (data[0] <= 252)
+            {
+                Number = data[0];
+                Bytes = new byte[] { data[0] };
+            }
+            else if (data[0] == 253 && data.Length >= (1 + sizeof(ushort))) //0xfd --> must be 2 bytes
+            {
+                Number = data.SubArray(1, sizeof(ushort)).ToUInt16(false);
+                if (Number <= 252)
+                {
+                    throw new FormatException("For values less than 253, one byte should be used.");
+                }
+                Bytes = data.SubArray(0, 1 + sizeof(ushort));
+            }
+            else if (data[0] == 254 && data.Length >= (1 + sizeof(uint))) //0xfe --> must be 4 bytes
+            {
+                Number = data.SubArray(1, sizeof(uint)).ToUInt32(false);
+                if (Number <= ushort.MaxValue)
+                {
+                    throw new FormatException($"For values less than {ushort.MaxValue}, two byte should be used.");
+                }
+                Bytes = data.SubArray(0, 1 + sizeof(uint));
+            }
+            else if (data[0] == 255 && data.Length >= (1 + sizeof(ulong))) //0xff --> must be 8 bytes
+            {
+                Number = data.SubArray(1, sizeof(ulong)).ToUInt64(false);
+                if (Number <= uint.MaxValue)
+                {
+                    throw new FormatException($"For values less than {uint.MaxValue}, four byte should be used.");
+                }
+                Bytes = data.SubArray(0, 1 + sizeof(ulong));
+            }
+            else
+            {
+                throw new FormatException("Invalid compact int format.");
+            }
         }
 
 
@@ -53,14 +106,42 @@ namespace CommonLibrary.CryptoEncoders
         public const int MaxSize = 1 + sizeof(ulong);
 
         /// <summary>
+        /// Minimum size of Compact Size Int. 1 byte.
+        /// </summary>
+        public const int MinSize = 1;
+
+        /// <summary>
         /// Array of bytes representing the CompactInt
         /// </summary>
-        public byte[] Bytes { get; set; }
+        public byte[] Bytes { get; private set; }
 
         /// <summary>
         /// Number representing the CompactInt
         /// </summary>
-        public ulong Number { get; set; }
+        public ulong Number { get; private set; }
+
+
+
+        private void SetFields(ulong num)
+        {
+            Number = num;
+            if (num <= 252) // 1 Byte
+            {
+                Bytes = new byte[] { (byte)num };
+            }
+            else if (num <= 0xffff) // 1 + 2 Byte
+            {
+                Bytes = new byte[] { 0xfd }.ConcatFast(((ushort)num).ToByteArray(false));
+            }
+            else if (num <= 0xffffffff) // 1 + 4 Byte
+            {
+                Bytes = new byte[] { 0xfe }.ConcatFast(((uint)num).ToByteArray(false));
+            }
+            else // < 0xffffffffffffffff // 1 + 8 Byte
+            {
+                Bytes = new byte[] { 0xff }.ConcatFast(((ulong)num).ToByteArray(false));
+            }
+        }
 
 
 
@@ -85,77 +166,6 @@ namespace CommonLibrary.CryptoEncoders
                 || (cInt[0] == 253 && cInt.Length >= size1 && cInt.SubArray(1, size1 - 1).ToUInt16(false) >= 253)
                 || (cInt[0] == 254 && cInt.Length >= size2 && cInt.SubArray(1, size2 - 1).ToUInt32(false) > ushort.MaxValue)
                 || (cInt[0] == 255 && cInt.Length >= size3 && cInt.SubArray(1, size3 - 1).ToUInt64(false) > uint.MaxValue);
-        }
-
-
-        /// <summary>
-        /// Reads a Compact Size Int. also returns size of the data that was used inside the byte array. 
-        /// <para/> byte array is little-endian
-        /// </summary>
-        /// <exception cref="ArgumentException"/>
-        /// <param name="data">Byte arrady starting with a Compact Size Int to read.</param>
-        /// <param name="size">Size of the Compact Size Int inside given <paramref name="data"/>.</param>
-        /// <returns>Unsigned integer equal to the Compact Size Int.</returns>
-        public static ulong Read(byte[] data, ref int size)
-        {
-            if (!StartsWithValidCompactInt(data))
-            {
-                throw new ArgumentException("Input is not a valid Compact Size Int.", nameof(data));
-            }
-
-            ulong result = 0;
-            if (data[0] <= 252)
-            {
-                result = data[0];
-                size = sizeof(byte);
-            }
-            else if (data[0] == 0xfd)
-            {
-                result = data.SubArray(1, sizeof(ushort)).ToUInt16(false);
-                size = 1 + sizeof(ushort);
-            }
-            else if (data[0] == 0xfe)
-            {
-                result = data.SubArray(1, sizeof(uint)).ToUInt32(false);
-                size = 1 + sizeof(uint);
-            }
-            else if (data[0] == 0xff)
-            {
-                result = data.SubArray(1, sizeof(ulong)).ToUInt64(false);
-                size = 1 + sizeof(ulong);
-            }
-
-            return result;
-        }
-
-
-        /// <summary>
-        /// Makes a variable length integer aka Compact Size Int.
-        /// <para/> byte array is little-endian
-        /// </summary>
-        /// <param name="num">Number to convert to Compact Size Int.</param>
-        /// <returns>Byte array of the Compact Size Int result in little endian order.</returns>
-        public static byte[] Write(ulong num)
-        {
-            byte[] result;
-            if (num <= 252) // 1 Byte
-            {
-                result = new byte[] { (byte)num };
-            }
-            else if (num <= 0xffff) // 1 + 2 Byte
-            {
-                result = new byte[] { 0xfd }.ConcatFast(((ushort)num).ToByteArray(false));
-            }
-            else if (num <= 0xffffffff) // 1 + 4 Byte
-            {
-                result = new byte[] { 0xfe }.ConcatFast(((uint)num).ToByteArray(false));
-            }
-            else // < 0xffffffffffffffff // 1 + 8 Byte
-            {
-                result = new byte[] { 0xff }.ConcatFast(((ulong)num).ToByteArray(false));
-            }
-
-            return result;
         }
 
     }
